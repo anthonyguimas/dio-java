e5a2087206629774f0f2801c14d4c4b662fb607b
anthonyguimas <anthonygabriel028@gmail.com>
2023-08-21 06:41:52 -0300

    Alteração no pull

---
+++ src/Sintaxe.java
@@ -0,0 +1,13 @@
+public class Sintaxe {
+    public static void main (String [] args) {
+
+      System.out.print("Anthony, teste");
+
+      String BR = "Brasil";
+      double PI = 3.14;
+      int ESTADOS_BRASILEIROS = 27;
+      int ANO_2000 = 2023;
+    }
+
+}
+
