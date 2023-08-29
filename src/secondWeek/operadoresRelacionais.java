1d77dbc8d49dc97adbf5bc5567d02f016c33b3c1
anthonyguimas <anthonygabriel028@gmail.com>
2023-08-29 19:35:06 -0300

    Operadores Relacionais

---
+++ src/secondWeek/operadoresRelacionais.java
@@ -0,0 +1,15 @@
+package secondWeek;
+
+public class operadoresRelacionais {
+
+    public static void main(String[] args) {
+        int number1 = 2;
+        int number2 = 3;
+
+        boolean yesNo = number1 == number2;
+
+        System.out.println("O número um é igual ao número 2?" + yesNo);
+
+
+    }
+}
