30385c310d51bd1ba6db807d9925f707b5e067b1
anthonyguimas <anthonygabriel028@gmail.com>
2023-09-09 11:01:16 -0300

    Nessa aula aprendi a como se utilizar o for em java,

---
+++ src/thirdWeek/ExampleFor.java
@@ -0,0 +1,12 @@
+package thirdWeek;
+
+public class ExampleFor {
+
+    public static void main(String[] args) {
+
+        for (int littleSheep = 1; littleSheep <= 50; littleSheep ++){
+            System.out.println("Contando os carneirinhos para dormir " + littleSheep);
+        }
+        System.out.println("André dormiu");
+    }
+}
---
+++ src/thirdWeek/ExampleForArray.java
@@ -0,0 +1,13 @@
+package thirdWeek;
+
+public class ExampleForArray {
+
+    public static void main(String[] args) {
+
+        String students[] = {"Anthony", "José", "Gilberto", "Gabigol"};
+
+        for (int x = 0; x < students.length;    x++){
+            System.out.println("O estudante no indice x= " + x + " é " + students[x]);
+        }
+    }
+}
